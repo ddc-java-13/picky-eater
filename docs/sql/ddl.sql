a537094cd53986914156392b47a480d15114d3bb
Kelley <kbonneville@cnm.edu>
2021-08-10 17:37:54 -0600

    wireframe 3 pt image

--- docs/sql/ddl.sql
+++ docs/sql/ddl.sql
@@ -7,14 +7,20 @@ CREATE TABLE IF NOT EXISTS `Recipe`
     `recipe_name`  TEXT,
     `cuisine`      TEXT,
     PRIMARY KEY (`recipe_id`)
-        CREATE INDEX IF NOT EXISTS `index_recipe_date_created` ON `Recipe` (`date_created`);
 
-CREATE INDEX IF NOT EXISTS `index_recipe_updated` ON `Recipe` (`updated`);
+        CREATE INDEX IF NOT EXISTS
+        `index_recipe_date_created` ON `Recipe` (`date_created`);
 
-CREATE INDEX IF NOT EXISTS `index_recipe_author` ON `Recipe` (`author`);
+CREATE INDEX IF NOT EXISTS
+    `index_recipe_updated` ON `Recipe` (`updated`);
 
-CREATE INDEX IF NOT EXISTS `index_recipe_recipe_name` ON `Recipe` (`recipe_name`);
+CREATE INDEX IF NOT EXISTS
+    `index_recipe_author` ON `Recipe` (`author`);
 
-CREATE INDEX IF NOT EXISTS `index_recipe_cuisine` ON `Recipe` (`cuisine`);
+CREATE INDEX IF NOT EXISTS
+    `index_recipe_recipe_name` ON `Recipe` (`recipe_name`);
+
+CREATE INDEX IF NOT EXISTS
+    `index_recipe_cuisine` ON `Recipe` (`cuisine`);
 
 
